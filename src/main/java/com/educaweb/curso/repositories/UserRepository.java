d5f4497eaa0eb115ff5fb044125ef2f97edb1b6d
abrahaomendes <abraaomneto@gmail.com>
2024-05-17 19:00:37 -0300

    Service layer, component registration

--- src/main/java/com/educaweb/curso/repositories/UserRepository.java
+++ src/main/java/com/educaweb/curso/repositories/UserRepository.java
@@ -2,10 +2,11 @@ package com.educaweb.curso.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.educaweb.curso.entities.User;
 
-
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
